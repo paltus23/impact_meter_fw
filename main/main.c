321acfa88289d0a79bcfb6109907bb6c83d040cd
paltus23 <paltusoff@gmail.com>
2026-03-21 11:32:09 +0400

    done the minimum required to make the project compile

--- main/main.c
+++ main/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "accel.h"
+#include "battery_measure.h"
+#include "data_storage.h"
+#include "http_server.h"
+#include "led.h"
+// #include "wifi.h"
 
 void app_main(void)
 {
-
+    // wifi_init();
+    data_storage_init();
+    battery_measure_init();
+    led_init();
+    accel_init();
+    http_server_init();
 }
